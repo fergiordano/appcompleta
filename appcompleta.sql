e9782621c8a2fd8790c337427ece8644ca40cdb7
fer <fergiordanofail@gmail.com>
2022-11-29 17:22:05 -0300

    practica de deploy sin database

--- appcompleta.sql
+++ appcompleta.sql
@@ -1,1 +1,33 @@
+
+
+-- Database para la App Completa --
 create database empresautn;
+
+-- Selecciono la Database -- 
+use empresautn;
+
+-- drop table empleados; --
+-- drop table contactos; --
+
+
+-- Creamos la tabla Productos para la App Completa --
+create table productos(
+idProducto int unsigned not null auto_increment,
+nombre varchar(150) not null,
+precio int not null,
+descripcion varchar(200) not null,
+primary key (idProducto)
+);
+
+-- Creamos la tabla Contacto para la App Completa --
+create table contactos(
+idContacto int unsigned not null auto_increment,
+nombre varchar(150) not null,
+email varchar(150) not null,
+primary key (idContacto)
+);
+
+
+
+
+
--- index.js
+++ index.js
@@ -1,52 +1,158 @@
-const express = require ('express');
-const app = express ();
-const mysql = require ('mysql2');
-//motor de plantilla
-const hbs = require ('hbs');
-//encontrar archivos 
-const path = require ('path');
-//para enviar mails
-const nodemailer = require ('nodemailer');
-//variables de entorno 
-require ('dotenv').config();
+const express = require('express');
+const app = express();
+const mysql = require('mysql2');
+//Motor de plantilla
+const hbs = require('hbs');
+//Encontrar archivos
+const path = require('path');
+//Para enviar mails
+const nodemailer = require('nodemailer');
+const { connect } = require('http2');
+const { connected } = require('process');
+//Variables de entorno
+require('dotenv').config();
 
+//Configuramos el Puerto
+const PORT = process.env.PORT || 9000; 
 
-//configuramos el puerto 
-const PORT = process.env.PORT || 9000;
-
-//middelware
+//Middelware
 app.use(express.json());
-app.use(express.urlencoded({extended: true}));
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, '/public')));
 
-//configuramos el motor de plantillas de hbs
+//Configuramos el motor de plantillas de HBS
 app.set('view engine', 'hbs');
-//configuramos la ubicacion de las plantillas
-app.set('view', path.join(__dirname, 'views/vistas'));
-//configuramos los parciales de los motores de plantillas
+//Configuramos la ubicación de las plantillas
+app.set('views', path.join(__dirname, 'views'));
+//Configuramos los parciales de los motores de plantillas
 hbs.registerPartials(path.join(__dirname, 'views/partials'));
 
-
-//conexion a la base de datos 
+//Conexión a la Base de Datos
 const conexion = mysql.createConnection({
     host: process.env.HOST,
     user: process.env.USER,
     password: process.env.PASSWORD,
     database: process.env.DATABASE,
-    port: process.env.DBPORT,
+    port: process.env.DBPORT
 })
 
-conexion.connect((err)=>{
+conexion.connect((err) =>{
     if(err) throw err;
-    console.log(`conectado a la database ${process.env.DATABASE}`);
+    console.log(`Conectado a la Database ${process.env.DATABASE}`);
 })
 
-//rutas de la aplicacion
-app.get('/', (req, res)=>{
-    res.send('bienvenido a la app completa')
+//Rutas de la Aplicación
+app.get('/', (req, res) => {
+    res.render('index', {
+        titulo: 'Home'
+    })
 })
 
-//servidor a la escucha de peticiones
+app.get('/formulario', (req, res) => {
+    
+    res.render('formulario')
+})
+
+app.get('/productos', (req, res) => {
+
+    let sql= "select * from productos";
+    conn.query (sql, function(err, result){
+        if (err) throw err;
+        console.log(result);
+        res.render ('productos',{
+            titulo: 'Productos',
+            datos: result
+        })
+        })
+    })
+
+
+
+app.get('/contacto', (req, res) =>{
+    res.render('contacto', {
+        titulo: 'Contacto'
+    })
+
+})
+
+app.post('/formulario', (req, res) =>{    
+    const nombre = req.body.nombre;
+    const precio = req.body.precio;
+    const descripcion = req.body.descripcion;
+
+    let datos = {
+        nombre: nombre,
+        precio: precio,
+        descripcion: descripcion
+    }
+
+    let sql = "INSERT INTO productos set ?";
+
+    conexion.query(sql, datos, function(err){
+        if (err) throw err;
+            console.log(`1 Registro insertado`);
+            res.render('enviado')
+        })
+})
+
+app.post('/contacto', (req, res) =>{
+    const nombre = req.body.nombre;
+    const email = req.body.email;
+
+    //Creamos una función para enviar Email al cliente
+    async function envioMail(){
+        //Configuramos la cuenta del envío
+        let transporter = nodemailer.createTransport({
+            host: 'smtp.gmail.com',
+            port: 465,
+            secure: true,
+            auth: {
+                user: process.env.EMAIL,
+                pass: process.env.EMAILPASSWORD
+            }
+        });
+
+        //Envío del mail
+        let info = await transporter.sendMail({
+            from: process.env.EMAIL,
+            to: `${email}`,
+            subject: "Gracias por suscribirte a nuestra App",
+            html:`Muchas gracias por visitar nuestra página <br>
+            Recibirás nuestras promociones a esta dirrección de correo. <br>
+            Buen fin de semana!!`
+        })
+    }
+
+    let datos = {
+        nombre: nombre,
+        email: email
+    }
+
+    let sql = "INSERT INTO contactos set ?";
+
+    conexion.query(sql, datos, function(err){
+        if (err) throw err;
+            console.log(`1 Registro insertado`);
+            //Email
+            envioMail().catch(console.error);
+            res.render('enviado')
+        })
+
+})
+
+//Servidor a la escucha de las peticiones
 app.listen(PORT, ()=>{
-    console.log(`servidor trabajando en el puerto: ${PORT}`);
+    console.log(`Servidor trabajando en el Puerto: ${PORT}`);
 })
+
+
+
+
+
+
+
+
+
+
+
+
